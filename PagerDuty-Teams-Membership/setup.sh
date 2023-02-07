4edc763e695ef0c7748f7143b2c2018111e06103
Rajesh Duraisamy <rajdnp@amazon.com>
2023-02-07 15:53:54 -0800

    Adding base cicd code and Teams membership to CICD

--- PagerDuty-Teams-Membership/setup.sh
+++ PagerDuty-Teams-Membership/setup.sh
@@ -3,5 +3,5 @@
 # Set up any prerequisites needed for cfn test
 #
 mkdir -p inputs
-cat example_inputs/inputs_1_create.json > inputs/inputs_1_create.json
-cat test/integ-template.yml  > test/integ.yml
+cat example_inputs/inputs_1_create.json | sed "s/PAGER_DUTY_TEAM_ID/${PAGER_DUTY_TEAM_ID}/g" | sed "s/PAGER_DUTY_USER_ID/${PAGER_DUTY_USER_ID}/g" > inputs/inputs_1_create.json
+cat test/integ-template.yml | sed "s/PAGER_DUTY_TEAM_ID/${PAGER_DUTY_TEAM_ID}/g" | sed "s/PAGER_DUTY_USER_ID/${PAGER_DUTY_USER_ID}/g" > test/integ.yml
